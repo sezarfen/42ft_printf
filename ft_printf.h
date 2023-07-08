de97424827a40e88d2cc7ec09e568c92c57c6246
sezarfen <furkandndr2014@gmail.com>
2023-07-08 12:34:28 +0300

    update

--- ft_printf.h
+++ ft_printf.h
@@ -7,7 +7,7 @@
 int	ft_printf(const char *format, ...);
 int	ft_putstr(char *str);
 int	ft_putchar(int c);
-int	ft_putptr(unsigned int ptr);
+int	ft_putptr(unsigned long ptr);
 int	ft_putnbr(int nb);
 int	ft_printuint(unsigned int u);
 int	ft_puthex(unsigned int u, char *hex);
